1912f66e7c7b78340bf59dec6f027c5f07e9e89c
sanavalos <santiago.avalos@outlook.com>
2024-01-24 22:15:29 -0300

    build: deploy in vercel

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
+require("dotenv").config();
+const {MONGODB_CONNECT_URI} = process.env
 const mongoose = require("mongoose");
-const uri =
-	"mongodb+srv://m001-student:m001-mongodb-basics@sandbox.yjdlxrm.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(uri, {
+mongoose.connect(MONGODB_CONNECT_URI, {
 	useNewUrlParser: true,
 	dbName: "sneakers",
 });
 
 mongoose.connection
 	.once("open", () => {
-		console.log("\n", "connect db", "\n");
+		console.log("\n", "Connected to db", "\n");
 	})
 	.on("error", (error) => {
 		console.log("\n", "Error in connect db:", error, "\n");
